acef190d0491aba04e939b3bbd0e61097e7519d3
jinbao <839302422@qq.com>
2016-06-22 16:21:53 +0800

    three

--- app/src/main/java/com/example/jinbao/testview/MainActivity.java
+++ app/src/main/java/com/example/jinbao/testview/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.jinbao.testview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this test
+        Log.e(TAG, "onCreate: ");
     }
 }
