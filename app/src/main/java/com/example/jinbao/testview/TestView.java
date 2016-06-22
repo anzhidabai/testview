c465542cb5f9db5e38e12355fef4d71aad81fdff
jinbao <839302422@qq.com>
2016-06-22 16:01:50 +0800

    *first commit

---
+++ app/src/main/java/com/example/jinbao/testview/TestView.java
@@ -0,0 +1,23 @@
+package com.example.jinbao.testview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by jinbao on 2016/6/22.
+ */
+public class TestView extends Button {
+
+    public TestView(Context context) {
+        super(context);
+    }
+
+    public TestView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
